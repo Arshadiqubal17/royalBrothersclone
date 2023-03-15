6d5d3981ecb10a87c81acc412eb679e88241b5da
amitsisodiya371 <asisodiya9045@gmail.com>
2023-03-15 23:10:45 +0530

    backend add

--- Backend_RB/Configs/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const connect = () =>{
-    console.log("Connected to the local database")
-    return mongoose.connect("");
-  }
-  
-  
-  module.exports = connect;
